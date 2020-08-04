4076a329d136edb0961ca87096050e1bcca26a2e
Ejike <victoryejike@gmail.com>
2020-08-04 12:02:57 +0100

    initial commit

---
+++ src/Components/Navbar-component/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { ReactComponent as Logo } from "../../assets/scam-alert.svg";
+
+const Navbar = () => {
+  return (
+    <div className="navbarContainer">
+      <Logo />
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "../Components/Navbar-component/Navbar";
+import Particles from "../Components/Particles-component/Particles";
+import Testimony from "../Components/Testimony-component/Testimony";
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <Particles />
+      <Testimony />
+    </div>
+  );
+};
+
+export default Home;
